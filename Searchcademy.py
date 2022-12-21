4436527fa3d024bcd379d3d41d50d998fbf2db50
Khaled Adad <khaledadad@lewisu.edu>
2022-12-21 10:36:26 -0600

    WIP

--- Searchcademy.py
+++ Searchcademy.py
@@ -2,7 +2,11 @@
 
 """
 Searchcademy
-In order to test a product, many companies use empty or “fake” data. Our company, Searchcademy, uses empty sparsely sorted data to test its awesome search engine. What exactly does that mean? Sparsely Sorted Data is data such that there is empty data in between the sorted values. For instance, an example dataset might look like:
+In order to test a product, many companies use empty or “fake” data. 
+Our company, Searchcademy, uses empty sparsely sorted data to test its 
+awesome search engine. What exactly does that mean? Sparsely Sorted Data 
+is data such that there is empty data in between the sorted values. 
+For instance, an example dataset might look like:
 
 ["Arthur", "", "", "", "", "Devan", "", "", "Elise", "", "", "", "Gary", "", "", "Mimi", "", "", "Parth", "", "", "", "Zachary"]
 In this project, we will implement a modified version of iterative binary search to search through a sparsely sorted dataset.
@@ -42,9 +46,22 @@ def sparse_search(data, search_val):
         print("{0} found at position {1}".format(search_val, mid))
         return None
 
+    if search_val < data[mid]:
+      last = mid - 1
 
+    if search_val > data[mid]:
+      first = mid + 1
+  
+  print("{0} is not in the dataset".format(search_val))
 
+################# TESTS #################
 
 sparse_search(["A", "", "", "", "B", "", "", "", "C"], "B")
 sparse_search(["A", "", "", "", ""], "A")
 sparse_search(["", "", "", "", "Z"], "Z")
+sparse_search(["A", "", "", "", "B", "", "", "", "C", "", "", "D"], "C")
+sparse_search(["A", "B", "", "", "E"], "A")
+sparse_search(["", "X", "", "Y", "Z"], "Z")
+sparse_search(["A", "", "", "", "B", "", "", "", "C"], "D")
+sparse_search(["Apple", "", "Banana", "", "", "", "", "Cow"], "Banana")
+sparse_search(["Alex", "", "", "", "", "Devan", "", "", "Elise", "", "", "", "Gary", "", "", "Mimi", "", "", "Parth", "", "", "", "Zachary"], "Parth")
