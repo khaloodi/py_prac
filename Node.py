14a42de351da84e8214cc101ab3cc39a7810a9c2
Khaled Adad <khaledadad@lewisu.edu>
2022-12-22 09:28:57 -0600

    add generic Node

---
+++ Node.py
@@ -0,0 +1,17 @@
+"""
+example of a generic Node class built using Python
+"""
+
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+  
+  def get_value(self):
+    return self.value
