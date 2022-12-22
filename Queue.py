7127d808208ec8970d5bb183dfd735731d233343
Khaled Adad <khaledadad@lewisu.edu>
2022-12-22 09:23:23 -0600

    wip

--- Queue.py
+++ Queue.py
@@ -3,3 +3,14 @@
 """
 Build a queue data structure
 """
+
+from node import Node
+# Create the Queue class below:
+
+class Queue(Node):
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    
+  def peek(self):
+    return self.head.get_value()
